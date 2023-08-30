b0b659aec3fda4fb7d6437f0eab1a7c648cd9416
Jose Garcia <betankore@gmail.com>
2023-08-30 11:02:41 -0400

    Return components to vue

---
+++ src/components/AppAlert.vue
@@ -0,0 +1,29 @@
+<template>
+  <div
+    class="w-full text-left py-4 lg:px-4 rounded flex items-center"
+    :class="{
+      'bg-primary': message.type === 'success',
+      'bg-red-700': message.type !== 'success',
+      'text-white': message.type !== 'success',
+    }"
+    role="alert"
+  >
+    <span class="mr-3">
+      <SocialIconsBase v-if="message.type === 'success'" icon="mdiCheck" />
+      <SocialIconsBase v-else icon="mdiAlert" />
+    </span>
+
+    <span> {message.text} </span>
+  </div>
+</template>
+
+<script setup>
+import SocialIconsBase from "./SocialIconsBase.vue";
+
+defineProps({
+  message: {
+    type: Object,
+    default: () => ({}),
+  },
+});
+</script>
---
+++ src/components/BaseCard.vue
@@ -0,0 +1,15 @@
+---
+---
+
+<template>
+  <div class="flex flex-col">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss">
+.card {
+  background-color: var(--bg-card);
+  border-radius: 10px;
+}
+</style>
