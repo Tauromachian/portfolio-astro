114934807b01529ff4020a04573e631f7c9fbeb3
Jose Garcia <betankore@gmail.com>
2023-08-30 08:40:27 -0400

    Make basic lang collection

---
+++ src/i18n/ui.js
@@ -0,0 +1,14 @@
+import en from "./en.js";
+import es from "./es.js";
+
+export const languages = {
+  en: "English",
+  es: "Spanish",
+};
+
+export const defaultLang = "en";
+
+export const ui = {
+  en,
+  es,
+};
