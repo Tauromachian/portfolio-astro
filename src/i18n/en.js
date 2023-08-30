a679dffbccc854daa524aa64570040906793e392
Jose Garcia <betankore@gmail.com>
2023-08-30 08:24:04 -0400

    Add lang

---
+++ src/i18n/en.js
@@ -0,0 +1,55 @@
+export default {
+  hero: {
+    title: "Making ideas happen",
+    description1: "I am a web developer/fullstacker",
+    description2: " I will build your site and make it look awesome",
+    button1: "Download CV",
+    button2: "Contact me",
+  },
+  home: "Home",
+  visit: "Visit",
+  about: "About",
+  aboutText1:
+    "Hello there! My name is José García. I am a fullstack developer. Design, databases, frontend, backend, SEO and multilanguage is what I do. I am a rounded dev with experience and dessire to prove myself in the field.",
+  aboutText2:
+    "I have been working in the field for over 3 years now. Helping small businesses grow and thrive. I have also worked for bigger companies providing my skills.",
+  aboutText3: "Let's discuss your needs, I'll start building right away.",
+  myPictureAlt: "My personal picture",
+  intuitive: "Intuitive",
+  intuitiveText: "User experience focused web sites",
+  fast: "Fast",
+  fastText: "The site's performance is a priority",
+  responsive: "Responsive",
+  responsiveText: "Web sites that work and look good in different screens",
+  reactive: "Reactive",
+  reactiveText: "Instant reactions to the user's interactions",
+  skills: "Skills",
+  contacts: "Contacts",
+  repositories: "Repositories",
+  formTitle: "Have a question or want to work together?",
+  testing: "Testing",
+  projects: {
+    title: "Projects",
+  },
+
+  additionalWork: {
+    title: "Additional work",
+  },
+
+  form: {
+    name: "Name",
+    email: "Email",
+    message: "Message",
+  },
+  button: {
+    send: "Send",
+  },
+  frontendDescription: "The visuals the beauty of the project.",
+  backendDescription:
+    "Part of the app the user doesn't see. The brains of the project.",
+  database: "Database",
+  databaseDescription: "The way you save data.",
+  testingDescription: "Make sure everything works as it should.",
+  language: "Languages",
+  languagesDescription: "The languages in which you can communicate with me.",
+};
