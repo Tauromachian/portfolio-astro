7f06ee274e44015291587bde32f2cf2216ec33b2
Tauromachian <54786476+Tauromachian@users.noreply.github.com>
2024-01-13 13:43:56 +0000

    Build: (2dcf2ad) Remove unnecessary icon

--- _astro/BaseButton.2dc555f2.js
+++
@@ -1,1 +0,0 @@
-import{B as i}from"./BaseButton.d8a62b2d.js";import"./_plugin-vue_export-helper.c27b6911.js";import"./runtime-core.esm-bundler.0bdc2b9e.js";/* empty css                       */export{i as default};
---
+++ _astro/BaseButton.ac88f419.js
@@ -0,0 +1,1 @@
+import{B as i}from"./BaseButton.3614f019.js";import"./_plugin-vue_export-helper.c27b6911.js";import"./runtime-core.esm-bundler.0bdc2b9e.js";/* empty css                       */export{i as default};
