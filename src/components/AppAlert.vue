9db614b0de49b4fa4a5a86c76a2882c4f617415f
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-01-13 15:09:07 -0500

    Use the simple success api

--- src/components/AppAlert.vue
+++ src/components/AppAlert.vue
@@ -2,20 +2,20 @@
   <div
     class="w-full text-left py-4 lg:px-4 rounded flex items-center"
     :class="{
-      'bg-primary': message.type === 'success',
-      'bg-red-700': message.type !== 'success',
-      'text-white': message.type !== 'success',
+      'bg-primary': message.success,
+      'bg-red-700': message.success,
+      'text-white': !message.success,
     }"
     role="alert"
   >
     <span class="mr-3">
-      <SocialIconsBase v-if="message.type === 'success'" icon="mdiCheck" />
+      <SocialIconsBase v-if="message.success" icon="mdiCheck" />
       <SocialIconsBase v-else icon="mdiAlert" />
     </span>
 
     <span> {{ message.text }} </span>
   </div>
-</template>
+</template
 
 <script setup>
 import SocialIconsBase from "./SocialIconsBase.vue";
