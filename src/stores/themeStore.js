7474f349353eece82d112b1e2b8658d2bca6e7d4
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-01-25 16:13:58 -0500

    Remove subscribe logic

--- src/stores/themeStore.js
+++ src/stores/themeStore.js
@@ -1,9 +1,4 @@
 import { atom } from "nanostores";
-import { setTheme } from "@/utils/themeHelper.js";
 
 export const selectedTheme = atom("default");
 export const themes = atom(["default", "dark", "crazy"]);
-
-selectedTheme.subscribe((selectedTheme) => {
-  setTheme(selectedTheme);
-});
