f323876fd96c6fa026a16734eeda420ddf4b4ba3
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-01-26 11:01:19 -0500

    Trim string

--- src/utils/themeHelper.js
+++ src/utils/themeHelper.js
@@ -1,4 +1,4 @@
 export function setTheme(theme) {
-  document.documentElement.setAttribute("theme", theme);
+  document.documentElement.setAttribute("theme", theme.trim());
   localStorage.setItem("theme", theme);
 }
