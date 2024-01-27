f53640d2b6d35854bf7aa03910462d886eb61ee8
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-01-27 11:19:31 -0500

    Add toggleTheme method

--- src/stores/themeStore.js
+++ src/stores/themeStore.js
@@ -2,3 +2,8 @@ import { atom } from "nanostores";
 
 export const selectedTheme = atom("default");
 
+export function toggleTheme() {
+  selectedTheme.value === "default"
+    ? selectedTheme.set("dark")
+    : selectedTheme.set("default");
+}
