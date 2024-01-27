f8727165d9def2d94eb075f55a4414e3240b0313
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-01-26 19:51:48 -0500

    Remove unnecessary file

--- src/stores/langStore.js
+++
@@ -1,9 +0,0 @@
-import { atom, computed } from "nanostores";
-
-import { useClientSideTranslations } from "../i18n/utils";
-
-export const lang = atom(null);
-export const t = computed(lang, (lang) => {
-  if (!lang) return function () {};
-  return useClientSideTranslations(lang);
-});
