57bb376d6c274a42c8621c7cf2e8bc2bff25451b
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-05-01 12:27:38 -0400

    Update years

--- src/i18n/es.js
+++ src/i18n/es.js
@@ -12,7 +12,7 @@ export default {
   aboutText1:
     "¡Hola! Mi nombre es José García. Soy desarrollador fullstack. Diseño, bases de datos, frontend, backend, SEO y multilenguaje es lo que hago.",
   aboutText2:
-    "He estado trabajando en el campo durante más de 3 años. Ayudando a las pequeñas empresas a crecer y prosperar. También he trabajado para empresas más grandes proporcionando mis habilidades.",
+    "He estado trabajando en el campo durante más de 4+ años. Ayudando a las pequeñas empresas a crecer y prosperar. También he trabajado para empresas más grandes proporcionando mis habilidades.",
   aboutText3: "¿Que necesitas?",
   myPictureAlt: "Mi foto personal",
   intuitive: "Intuitivo",
