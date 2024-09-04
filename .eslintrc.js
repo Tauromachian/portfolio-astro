041cc58376eec57a9b14a58bc5d5470f87927565
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-09-03 20:55:15 -0400

    Update eslint file type

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-/* eslint-env node */
-modules.exports = {
-  root: true,
-  extends: ["plugin:astro/recommended", "plugin:prettier/recommended"],
-  parserOptions: {
-    ecmaVersion: "latest",
-  },
-};
