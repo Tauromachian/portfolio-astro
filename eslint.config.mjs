cccad2aa5dcc3d8d544a85fc7999c9ec09d25af9
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2026-02-17 16:31:15 +0100

    Ignore built in files in eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,13 +2,16 @@ import eslintPluginAstro from "eslint-plugin-astro";
 import eslintPluginPrettier from "eslint-plugin-astro";
 
 export default [
-    ...eslintPluginPrettier.configs.recommended,
-    ...eslintPluginAstro.configs.recommended,
-    {
-        rules: {
-            // override/add rules settings here, such as:
-            // "astro/no-set-html-directive": "error"
-        },
-    },
+  ...eslintPluginPrettier.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
+  {
+    ignores: [
+      ".astro/**",
+      "dist/**",
+      "node_modules/**",
+    ],
+  },
+  {
+    rules: {},
+  },
 ];
-
